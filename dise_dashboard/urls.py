7d0bf62009c62459aeed3b64ca38cf9dbb4a0210
Dinesh kumar Sharma <dinesh829269@gmail.com>
2017-08-08 21:21:12 +0530

    URL Modified

--- dise_dashboard/urls.py
+++ dise_dashboard/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
+from django.views.generic import RedirectView
 admin.autodiscover()
 
 from schools.views import HomeView
 
 urlpatterns = patterns('',
-    url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    #url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^$', RedirectView.as_view(url='/site')),
+    #url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
-    url(r'^api/', include('dise_dashboard.api_urls')),
+    #url(r'^api/', include('dise_dashboard.api_urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^site/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^explorer/', include('explorer.urls')),
 )
